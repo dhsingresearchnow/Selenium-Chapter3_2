86ec7ca808e507ff5bff5707734fa528e460c63d
D Hsing <dhsing@researchnow.com>
2016-03-16 12:27:43 -0500

    all java files for chapter3_2

---
+++ src/test/java/packt/selenium/chap3_2/SeleniumHQ.java
@@ -0,0 +1,27 @@
+package packt.selenium.chap3_2;
+
+// import org.apache.xpath.operations.String;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Ripon on 5/14/2015.
+ */
+public class SeleniumHQ {
+    private WebDriver driver;
+    private String baseUrl;
+
+    public SeleniumHQ(WebDriver driver) {
+        this.driver = driver;
+        driver.manage().window().maximize();
+        baseUrl = "http://www.seleniumhq.org";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get(baseUrl + "/");
+    }
+
+    public Documentation clickDocumentation(){
+        driver.findElement(By.linkText("Documentation")).click();
+        return new Documentation(driver);
+    }
+}
